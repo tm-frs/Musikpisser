c713da630bf6f4c31ad7847fdd8c01a38cd323a6
Glitch (maize-stream-armchair) <none>
2022-01-30 15:42:18 +0000

    👏🐲 Checkpoint ./config.js:41694393/40

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
         px: '!',
-        playing: 'Music Bot',
+        playing: 'Music Bot || !help for help',
 
     opt: {
         DJ: {
-            enabled: false, //IF YOU WANT ONLY DJS TO USE IT, set false to true.
-            roleName: 'DJ', //WRITE WHAT THE NAME OF THE DJ ROLE WILL BE, THEY CAN USE IT ON YOUR SERVER
+            enabled: true, //IF YOU WANT ONLY DJS TO USE IT, set false to true.
+            roleName: 'Music', //WRITE WHAT THE NAME OF THE DJ ROLE WILL BE, THEY CAN USE IT ON YOUR SERVER
             commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume'] //Please don't touch
         },
         maxVol: 250, //You can specify the maximum volume level.
