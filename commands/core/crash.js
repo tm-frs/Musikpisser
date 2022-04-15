ad01e4a3669cf7a513c3024fc40dd9f0f60db837
TmFrs <82935664+tm-frs@users.noreply.github.com>
2022-04-16 01:27:00 +0200

    created a crash command thats not shown in command list

---
+++ commands/core/crash.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'crash',
+    aliases: ['restart','error'],
+    showHelp: false,
+    utilisation: '{prefix}crash',
+
+      execute(client, message, args) {
+      dgdfhfhjgjh // just some random things that are just here to crash the bot if needed
+    },
+};
