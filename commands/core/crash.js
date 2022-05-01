a6bc7aa405c12753464fe989956a3e6ad10a594d
TmFrs <82935664+tm-frs@users.noreply.github.com>
2022-05-01 13:10:03 +0200

    You can now give crashperms (now adminperms) to multiple users

--- commands/core/crash.js
+++ commands/core/crash.js
@@ -1,4 +1,4 @@
-const crashperms = require("../../config.js").opt.crashperms; // gets the name of the user that is allowed to do this from config file
+const adminperms = require("../../config.js").opt.adminperms; // gets the name of the user that is allowed to do this from config file
 
 module.exports = {
     name: 'crash',
@@ -9,10 +9,10 @@ module.exports = {
       execute(client, message, args) {
         const messagecreator = (message.author.username+'#'+message.author.discriminator); // saves the name of the user executing this as a variable
         console.log('Crash command executed by: '+messagecreator); // will let you see in the logs who used the command 
-        if (messagecreator === crashperms) { // checks if user is allowed to let the bot crash
+        if (adminperms.includes(messagecreator)) { // checks if user is allowed to let the bot crash
       dgdfhfhjgjh // just some random things that are just here to crash the bot if needed
         } else {
-          console.log('Crash command not executed because only '+crashperms+' is allowed to do it, and '+messagecreator+' is not.'); // writes in logs if command failed because there are no permissions
+          console.log('Crash command not executed because '+messagecreator+' has no permission. \nThe following user(s) is/are allowed to crash the bot:\n'+adminperms); // writes in logs if command failed because there are no permissions
           return message.channel.send(`${message.author}, You are not allowed to do this! ❌`); // tells the user he is not allowed to let the bot crash
           
         }
