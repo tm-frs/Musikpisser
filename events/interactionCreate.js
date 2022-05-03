4549babe7e4b17b74ea8b2ddffa7c44a10353888
CraftingShadowDE <82935664+CraftingShadowDE@users.noreply.github.com>
2022-05-03 20:43:21 +0200

    the bot is now sending you an URL when clicking "save" at nowplaying cmd

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -7,7 +7,7 @@ module.exports = (client, int) => {
         case 'saveTrack': {
           if (!queue || !queue.playing) return int.reply({ content: `No music currently playing. ❌`, ephemeral: true, components: [] });
 
-            int.member.send(`**Track Saved: \`${queue.current.title}\` | Posted by \`${queue.current.author}\`, Saved Server: \`${int.member.guild.name}\` ✅**`).then(() => {
+            int.member.send(`**Track Saved: \`${queue.current.title}\`, Posted by \`${queue.current.author}\`, URL: <${queue.current.url}> | Saved Server: \`${int.member.guild.name}\` ✅**`).then(() => {
                 return int.reply({ content: `I sent you the name of the music in a private message ✅`, ephemeral: true, components: [] });
             }).catch(error => {
                 return int.reply({ content: `I can't send you a private message. ❌`, ephemeral: true, components: [] });
