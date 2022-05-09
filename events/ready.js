1f63380e0fa003a558c4b16f3bc44558f382e05d
CraftingShadowDE <82935664+CraftingShadowDE@users.noreply.github.com>
2022-05-09 16:54:44 +0200

    added count of connected servers to console log and playing info

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,6 @@
 module.exports = async (client) => {
     console.log(`${client.user.username} Login!`);
+    console.log('Connected servers: '+client.guilds.cache.size)
 
-    client.user.setActivity(client.config.playing);
+    client.user.setActivity(client.config.playing+" || Online on "+client.guilds.cache.size+" servers");
 };
