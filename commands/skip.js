8a8905ca244921cb5d47b74111e0fc19623e220b
TmFrs <82935664+tm-frs@users.noreply.github.com>
2022-05-22 20:22:05 +0200

    fixed a bug that when loop track is active, it won't skip

--- commands/skip.js
+++ commands/skip.js
@@ -1,3 +1,5 @@
+const { QueueRepeatMode } = require('discord-player');
+
 module.exports = {
     description: "Skips the current track and starts the nest track in queue.",
     name: 'skip',
@@ -9,8 +11,18 @@ module.exports = {
  
         if (!queue || !queue.playing) return interaction.reply({ content: `No music currently playing! ❌`, ephemeral: true }).catch(e => { });
 
+      if (queue.repeatMode===1){
+        queue.setRepeatMode(QueueRepeatMode.OFF);
+        setTimeout(function() {
+          const success = queue.skip();
+          return interaction.reply({ content: success ? `**${queue.current.title}**, the song currently playing, has been skipped ✅` : `Something went wrong ❌` }).catch(e => { });
+        }, 500);
+        setTimeout(function() {
+          queue.setRepeatMode(QueueRepeatMode.TRACK);
+        }, 1000);
+      } else {
         const success = queue.skip();
-
         return interaction.reply({ content: success ? `**${queue.current.title}**, the song currently playing, has been skipped ✅` : `Something went wrong ❌` }).catch(e => { });
+      }
     },
 };
