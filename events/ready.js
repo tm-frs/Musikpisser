03f0fe0dce9716c27f9bbd8d88a91fb4b32938e0
CraftingShadowDE <82935664+CraftingShadowDE@users.noreply.github.com>
2022-05-22 16:45:09 +0200

    changed user activity from 'listening' to 'playing'

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,11 @@
 module.exports = async (client) => {
     console.log(`${client.user.username} Login!`);
     client.user.setActivity(client.config.playing+` || Online on ${client.guilds.cache.size} servers`, {
-        type: "LISTENING"
+        type: "PLAYING"
       })
       setInterval(() => {
         client.user.setActivity(client.config.playing+` || Online on ${client.guilds.cache.size} servers`, {
-            type: "LISTENING"
+            type: "PLAYING"
           })
       }, 600000)
 };
