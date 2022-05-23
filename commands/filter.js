042b95af538976c9953f4f9432c9e5eff932ed5e
TmFrs <82935664+tm-frs@users.noreply.github.com>
2022-05-23 16:09:40 +0200

    added ALL filters that are included in discord-player (befor, only 8D, nightcore and bassboost were available)

--- commands/filter.js
+++ commands/filter.js
@@ -1,16 +1,53 @@
 module.exports = {
     description: "Adds audio filter to ongoing music.",
     name: 'filter',
-    options: [ {
-        name: 'filterinput',
-        description: "Choose the filter you want to apply.",
-        type: 'STRING',
-		choices: [
-		{value: 'bassboost', name: "Bassboost"},
-		{value: '8D', name: "8D"},
-		{value: 'nightcore', name: "Nightcore"}
-		],
-        required: true
+    options: [{
+    name: 'filter-list_1',
+    description: "The first part of the list of all available filters. Only choose one from one of the lists!",
+    type: 'STRING',
+	choices: [
+    {name: "Bassboost low (+15dB)", value: 'bassboost_low'},
+	{name: "Bassboost normal (+20dB)", value: 'bassboost'},
+    {name: "Bassboost high (+30dB)", value: 'bassboost_high'},
+	{name: "8D", value: '8D'},
+    {name: "Vaporwave", value: 'vaporwave'},
+	{name: "Nightcore", value: 'nightcore'},
+    {name: "Phaser", value: 'phaser'},
+    {name: "Tremolo", value: 'tremolo'},
+    {name: "Vibrato", value: 'vibrato'},
+    {name: "Reverse", value: 'reverse'},
+    {name: "Treble", value: 'treble'},
+    {name: "Normalizer 1 (dynamic audio normalizer based)", value: 'normalizer'},
+    {name: "Normalizer 2 (audio compressor based)", value: 'normalizer2'},
+    {name: "Surrounding", value: 'surrounding'},
+    {name: "Pulsator", value: 'pulsator'},
+    {name: "Subboost", value: 'subboost'},
+    {name: "Karaoke", value: 'karaoke'},
+    {name: "Flanger", value: 'flanger'},
+    {name: "Gate", value: 'gate'},
+    {name: "Haas", value: 'haas'},
+    {name: "Mcompand", value: 'mcompand'},
+    {name: "Mono", value: 'mono'},
+    {name: "Mstlr", value: 'mstlr'},
+    {name: "Mstrr", value: 'mstrr'},
+    {name: "Compressor", value: 'compressor'} //25
+	],
+    required: false
+    },{
+    name: 'filter-list_2',
+    description: "The second part of the list of all available filters. Only choose one from one of the lists!",
+    type: 'STRING',
+	choices: [
+    {name: "Expander", value: 'expander'},
+    {name: "Softlimiter", value: 'softlimiter'},
+    {name: "Chorus", value: 'chorus'},
+    {name: "Chorus 2D", value: 'chorus2d'},
+    {name: "Chorus 3D", value: 'chorus3d'},
+    {name: "Fadein", value: 'fadein'},
+    {name: "Dim", value: 'dim'},
+    {name: "Earrape", value: 'earrape'} //8
+	],
+    required: false
     }],
     voiceChannel: true,
 
@@ -18,10 +55,19 @@ module.exports = {
         const queue = client.player.getQueue(interaction.guild.id);
 
    if (!queue || !queue.playing) return interaction.reply({ content: `No music currently playing! ❌`, ephemeral: true }).catch(e => { });
-   const filterinput = interaction.options.getString('filterinput')
+   const filterinput1 = interaction.options.getString('filter-list_1')
+   const filterinput2 = interaction.options.getString('filter-list_2')
+   
+   if (filterinput1 && filterinput2) return interaction.reply({ content: `Only choose a filter from **one** list! ❌`, ephemeral: true }).catch(e => { });
+   if (!filterinput1 && !filterinput2) return interaction.reply({ content: `Choose a filter from one of the lists! ❌`, ephemeral: true }).catch(e => { });
+   const list1 = (filterinput1 && !filterinput2)
+   const list2 = (!filterinput1 && filterinput2)
+      
+        const filterinput = list1 ? filterinput1 : list2 ? filterinput2 : '';
+        
         const actualFilter = queue.getFiltersEnabled()[0];
 
-        if (!filterinput) return interaction.reply({ content: `Please enter a valid filter name. ❌\n\`bassboost, 8D, nightcore\``, ephemeral: true }).catch(e => { });
+        if (!filterinput) return interaction.reply({ content: `Please enter a valid filter name. ❌`, ephemeral: true }).catch(e => { });
 
         const filters = [];
         queue.getFiltersEnabled().map(x => filters.push(x));
@@ -29,7 +75,7 @@ module.exports = {
 
         const filter = filters.find((x) => x.toLowerCase() === filterinput.toLowerCase());
 
-        if (!filter) return interaction.reply({ content: `I couldn't find a filter with your name. ❌\n\`bassboost, 8D, nightcore\``, ephemeral: true }).catch(e => { });
+        if (!filter) return interaction.reply({ content: `I couldn't find a filter with your name. ❌`, ephemeral: true }).catch(e => { });
 
         const filtersUpdated = {};
 
@@ -37,6 +83,6 @@ module.exports = {
 
         await queue.setFilters(filtersUpdated);
 
-        interaction.reply({ content: `Applied: **${filter}**, Filter Status: **${queue.getFiltersEnabled().includes(filter) ? 'Active' : 'Inactive'}** ✅\n **Remember, if the music is long, the filter application time may be long.**` }).catch(e => { });
+        interaction.reply({ content: `Changed: **${filter}**, New filter status: **${queue.getFiltersEnabled().includes(filter) ? 'Active' : 'Inactive'}** ✅\n**Remember, if the track is long, the filter application time may be long too.**` }).catch(e => { });
     },
 };
