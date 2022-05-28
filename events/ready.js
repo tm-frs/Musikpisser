b84257e91126c86d2c7cccee3c5f477d5c1da111
TmFrs <82935664+tm-frs@users.noreply.github.com>
2022-05-28 15:00:38 +0200

    fixed the activity status' timestamp

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,14 @@
 module.exports = async (client) => {
     const unixReadyAt = Math.floor(new Date(client.readyAt).getTime() / 1000);
     const jsReadyAt = new Date(unixReadyAt * 1000);
+    const activityText = `${client.config.playing} || Online on ${client.guilds.cache.size} servers || Logged in: ${jsReadyAt.toUTCString()}`;
     console.log(`------------------------LOGIN-INFORMATION------------------------\n${client.user.username} Login! Login at: \n${jsReadyAt}\n-----------------------------------------------------------------`);
     client.user.accentColor = '#18191C';
-    client.user.setActivity(client.config.playing+` || Online on ${client.guilds.cache.size} servers || Logged in: ${jsReadyAt.toUTCString()}`, {
+    client.user.setActivity(activityText, {
         type: "PLAYING"
       })
       setInterval(() => {
-        client.user.setActivity(client.config.playing+` || Online on ${client.guilds.cache.size} servers`, {
+        client.user.setActivity(activityText, {
             type: "PLAYING"
           })
       }, 600000)
