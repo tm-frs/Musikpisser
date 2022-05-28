fabebb7366f88c81af3d5bdd8855f07a7ed0c49f
TmFrs <82935664+tm-frs@users.noreply.github.com>
2022-05-28 21:05:49 +0200

    added a log file for login information

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,18 @@
+var fs = require('fs');
+var util = require('util');
+var logFile = fs.createWriteStream('logins.txt', { flags: 'as' });
+
+const writefile = function () {
+  logFile.write(util.format.apply(null, arguments) + '\n');
+}
+
 module.exports = async (client) => {
     const unixReadyAt = Math.floor(new Date(client.readyAt).getTime() / 1000);
     const jsReadyAt = new Date(unixReadyAt * 1000);
     const activityText = `${client.config.playing} || Online on ${client.guilds.cache.size} servers || Logged in: ${jsReadyAt.toUTCString()}`;
-    console.log(`------------------------LOGIN-INFORMATION------------------------\n${client.user.username} Login! Login at: \n${jsReadyAt}\n-----------------------------------------------------------------`);
+    const loginText = `------------------------LOGIN-INFORMATION------------------------\n${client.user.username} Login! Login at: \n${jsReadyAt}\n-----------------------------------------------------------------`;
+    writefile(`\n${loginText}\n`);
+    console.log(loginText);
     client.user.accentColor = '#18191C';
     client.user.setActivity(activityText, {
         type: "PLAYING"
