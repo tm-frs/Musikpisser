7e8df971d3b5dd623e78ee500ab20ec688cc8099
CraftingShadowDE <82935664+CraftingShadowDE@users.noreply.github.com>
2022-05-31 17:33:04 +0200

    changed /ping

--- commands/ping.js
+++ commands/ping.js
@@ -7,18 +7,18 @@ module.exports = {
 
     run: async (client, interaction) => {
         const start = Date.now();
-        interaction.reply('Pong!').then(async() => {
+        interaction.reply('Please wait...').then(async() => {
         let last = Date.now();
             const embed = new Discord.MessageEmbed()
                 .setColor('BLUE')
-                .setTitle(client.user.username + " - Pong!")
+                .setTitle(client.user.username + " - Current Ping")
                 .setThumbnail(client.user.displayAvatarURL({ format: 'png', size: 4096 }))
                 .addField(`Message Ping`, `\`${Date.now() - start}ms\` 🛰️`)
                 .addField(`Message Latency`, `\`${last - start}ms\` 🛰️`)
                 .addField(`API Latency`, `\`${Math.round(client.ws.ping)}ms\` 🛰️`)
                 .setTimestamp()
                 .setFooter({ text: 'Music Bot - by ️CraftingShadowDE', iconURL: interaction.user.displayAvatarURL({ dynamic: true }) });
-            interaction.editReply({ embeds: [embed] }).catch(e => { });
+            interaction.editReply({ content: null, embeds: [embed] }).catch(e => { });
         })
     },
 };
