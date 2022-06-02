3d963abb8b30d16bbdd0a2ded63b47cb0e7d31e6
CraftingShadowDE <82935664+CraftingShadowDE@users.noreply.github.com>
2022-06-02 22:03:09 +0200

    you now can set the status of the bot
    "online", "idle", "dnd" (do not desturb) and "invisible"

--- events/ready.js
+++ events/ready.js
@@ -10,17 +10,25 @@ module.exports = async (client) => {
     const unixReadyAt = Math.floor(new Date(client.readyAt).getTime() / 1000);
     const jsReadyAtShort = ((new Date(unixReadyAt * 1000)).toUTCString()).replace("GMT", "(UTC+0)");
     const jsReadyAtLong = ((new Date(unixReadyAt * 1000)).toUTCString()).replace("GMT", "UTC+0000 (Coordinated Universal Time)");
-    const activityText = `${client.config.playing} || Online on ${client.guilds.cache.size} servers || Logged in: ${jsReadyAtShort}`;
     const loginText = `------------------------LOGIN-INFORMATION------------------------\n${client.user.username} Login! Login at: \n${jsReadyAtLong}\n-----------------------------------------------------------------`;
     writefile(`\n${loginText}\n`);
     console.log(loginText);
     client.user.accentColor = '#18191C';
+    const status = 'online' // the status of the bot (options: "online", "idle", "dnd" (do not desturb), "invisible")
+    const activityText = `${client.config.playing} || Online on ${client.guilds.cache.size} servers || Logged in: ${jsReadyAtShort}`; //the text of the activity
+    const activityType = 'PLAYING' // the type of the activity (options: "PLAYING", "LISTENING", "WATCHING", "COMPETING", "STREAMING")
+    client.user.setPresence({
+      status: status,
+    })
     client.user.setActivity(activityText, {
-        type: "PLAYING"
-      })
+      type: activityType
+    })
       setInterval(() => {
+        client.user.setPresence({
+          status: status,
+        })
         client.user.setActivity(activityText, {
-            type: "PLAYING"
-          })
+          type: activityType
+        })
       }, 600000)
 };
