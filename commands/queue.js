a0bdafc72650f3d8c59b7018ba7f58e963ea3dad
CraftingShadowDE <82935664+CraftingShadowDE@users.noreply.github.com>
2022-06-18 23:58:13 +0200

    added a text to show since when the bot is playing
    added this to /time and /queue

--- commands/queue.js
+++ commands/queue.js
@@ -1,4 +1,5 @@
 const { MessageEmbed } = require('discord.js');
+const { SnowflakeUtil } = require('discord.js');
 const { QueueRepeatMode } = require('discord-player');
 
 module.exports = {
@@ -16,6 +17,9 @@ module.exports = {
 
         if (!queue.tracks[0]) return interaction.reply({ content: `No music in queue after current. ❌`, ephemeral: true }).catch(e => { });
 
+        const unixPlayingSince = parseInt((SnowflakeUtil.deconstruct(queue.id).timestamp)/1000);
+        const discordPlayingSince = `<t:${unixPlayingSince}:R> (<t:${unixPlayingSince}:d>, <t:${unixPlayingSince}:T>)`
+
         const embed = new MessageEmbed();
 		const options = ['📴 (Loop mode: Off)','🔂 (Loop mode: Track)','🔁 (Loop mode: Queue)','▶ (Loop mode: Autoplay)']
         const loopMode = options[queue.repeatMode];
@@ -29,7 +33,7 @@ module.exports = {
         const songs = queue.tracks.length;
         const nextSongs = songs > 5 ? `And **${songs - 5}** Other Song...` : `There are **${songs}** Songs in the List.`;
 
-        embed.setDescription(`Currently Playing: \`${queue.current.title}\` by \`${queue.current.author}\` (requested by <@${queue.current.requestedBy.id}>)\n\n${tracks.slice(0, 5).join('\n')}\n\n${nextSongs }`);
+        embed.setDescription(`The bot is playing since: *${discordPlayingSince}*.\n\n**Currently Playing:** \`${queue.current.title}\` by \`${queue.current.author}\` (requested by <@${queue.current.requestedBy.id}>)\n\n${tracks.slice(0, 5).join('\n')}\n\n${nextSongs }`);
 
         embed.setTimestamp();
         embed.setFooter({text: 'Music Bot - by CraftingShadowDE', iconURL: interaction.user.displayAvatarURL({ dynamic: true }) });
