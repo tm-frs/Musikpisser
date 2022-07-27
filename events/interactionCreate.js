f764967e13495d50cb9ddaa4cbdf66ebca0f7ea4
CraftingShadowDE <82935664+CraftingShadowDE@users.noreply.github.com>
2022-07-27 23:10:50 +0200

    made /queue better

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -164,12 +164,12 @@ const othervoicechannel = (botvoicechannel && int.member.voice.channel.id !== bo
         embed.setThumbnail(int.guild.iconURL({ size: 4096, format: 'png', dynamic: true }));
         embed.setTitle(`Server Music List - ${int.guild.name} ${loopMode}`);
   
-        const tracks = queue.tracks.map((track, i) => `**${i + 1}** - \`${track.title}\` | \`${track.author}\` (requested by <@${track. requestedBy.id}>)`);
+        const tracks = queue.tracks.map((track, i) => `**${i + 1}**. \`${track.title}\` | by \`${track.author}\` _(Duration: **${track.duration}**)_\n(requested by <@${track. requestedBy.id}>)`);
   
         const songs = queue.tracks.length;
-        const nextSongs = songs > 5 ? `And **${songs - 5}** Other Song...` : `There are **${songs}** Songs in the List.`;
+        const nextSongs = songs > 5 ? `There ${((songs-5)!==1)?"are":"is"} **${songs - 5} other song${((songs-5)!==1)?"s":""}** in the queue.` : `There are **no other songs** in the queue.`;
   
-        embed.setDescription(`The bot is playing since: *${discordPlayingSince}*.\n\n**Currently Playing:** \`${queue.current.title}\` by \`${queue.current.author}\` (requested by <@${queue.current.requestedBy.id}>)\n\n${tracks.slice(0, 5).join('\n')}\n\n${nextSongs }`);
+        embed.setDescription(`The bot is playing since: *${discordPlayingSince}*.\n\n**Currently Playing:** \`${queue.current.title}\` | by \`${queue.current.author}\` _(Duration: **${queue.current.duration}**)_ (requested by <@${queue.current.requestedBy.id}>)\n\n${tracks.slice(0, 5).join('\n')}\n\n${nextSongs }`);
   
         embed.setTimestamp();
         embed.setFooter({text: 'Music Bot - by CraftingShadowDE', iconURL: int.user.displayAvatarURL({ dynamic: true }) });
