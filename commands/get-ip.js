75a51bb60f26a7ca5ac5977147446fc7360ef17b
CraftingShadowDE <82935664+CraftingShadowDE@users.noreply.github.com>
2022-09-08 23:11:45 +0200

    added an option for permitted users to show bot ip

---
+++ commands/get-ip.js
@@ -0,0 +1,42 @@
+const adminperms = require("../config.js").opt.adminperms; // gets the name of the user that is allowed to do this from config file
+const netTools = require("../netTools.js");
+const { Colors } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	description: "Get the bot's IP (permitted users only).",
+    name: 'get-ip',
+    options: [],
+    showHelp: false, // makes the command not show in help command
+
+    run: async (client, interaction) => {
+        await interaction.deferReply({ ephemeral: true });
+
+        const messagecreator = (interaction.member.user.username+'#'+interaction.member.user.discriminator); // saves the name of the user executing this as a variable
+        console.log('Get-IP command executed by: '+messagecreator); // will let you see in the logs who used the command 
+        
+        if (adminperms.includes(messagecreator)) { // checks if user is allowed to get the bot's IP
+          
+          const publicIP = await netTools.getPublicIp();
+          const privateIP = await netTools.getPrivateIp();
+          const port = await netTools.getPort();
+
+          const embed = new EmbedBuilder()
+          .setColor(Colors.Blue) // blue = 0x3498DB
+          .setTitle('Bot IP')
+          .setThumbnail(client.user.displayAvatarURL({ format: 'png', size: 4096 }))
+          .setDescription(`The bot's current public and private IP:`)
+          .addFields([{ name: `Private IP:`, value: `${privateIP}:${port}` }, { name: `Public IP:`, value: `${publicIP}` }])
+          .setTimestamp()
+          .setFooter({ text: 'Music Bot - by CraftingShadowDE', iconURL:interaction.user.displayAvatarURL({ dynamic: true }) })
+
+          await interaction.editReply({ embeds: [embed], content: `Here it is! ✅ Be careful with this!`, ephemeral: true }).catch(e => { });
+
+        } else {
+
+          console.log(`Get-IP command not executed because `+messagecreator+` has no permission. \nThe following user(s) is/are allowed to get the bot's IP:\n`+adminperms); // writes in logs if command failed because there are no permissions
+          return interaction.editReply({ content: `You are not allowed to do this! ❌`, ephemeral: true }).catch(e => { }); // tells the user he is not allowed to get the bot's IP
+
+        }
+    },
+};
---
+++ netTools.js
@@ -0,0 +1,43 @@
+const defaultApi = require("./config.js").defaultPublicIpApi;
+var https = require('https');
+var ip = require("ip");
+require('dotenv').config();
+
+const getPublicIp = async (apiUrlInput) => {
+    const apiURL = (apiUrlInput) ? `${apiUrlInput}` : defaultApi;
+    
+    var toReturn = new Promise((resolveReturn, rejectReturn) => {
+        https.get({
+            host: apiURL,
+        }, function(response) {
+            var ip = '';
+            response.on('data', function(d) {
+                ip += d;
+            });
+            response.on('end', function() {
+                if(ip){
+                    resolveReturn(`${ip}`);
+                } else {
+                    resolveReturn(`ERROR: Couldn't get public IP! ❌`);
+                }
+            });
+        });
+    });
+
+    return toReturn;
+}
+
+const getPrivateIp = async () => {
+    const toReturn = await ip.address();
+    return toReturn;
+}
+
+const getPort = function() {
+    return (process.env['PORT'] || 3001);
+}
+
+module.exports = {
+    getPublicIp,
+    getPrivateIp,
+    getPort
+};
