a9397feebdca347489e26743a33671119be03e52
CraftingShadowDE <82935664+CraftingShadowDE@users.noreply.github.com>
2022-09-08 21:55:38 +0200

    nothing changed, just made VS-Code stop crying

--- commands/help.js
+++ commands/help.js
@@ -25,7 +25,7 @@ module.exports = {
 //		.addField(`**DJ mode active:** `, DJ.enabled) //if reactivated, convert to addFields
 //		.addField(`**Available for everyone:** `, DJ.notAffected) //if reactivated, convert to addFields
         .setTimestamp()
-        .setFooter({ text: 'Music Bot - by ️CraftingShadowDE', iconURL:interaction.user.displayAvatarURL({ dynamic: true }) })
+        .setFooter({ text: 'Music Bot - by CraftingShadowDE', iconURL:interaction.user.displayAvatarURL({ dynamic: true }) })
         interaction.reply({ embeds: [embed] }).catch(e => { })
     },
 };
